8805763ab3232d2626dc6284e55f6f38bcaf53ee
gomtange12 <blueballoon1@naver.com>
2019-01-22 18:17:20 +0900

    22일 모델커밋

--- Project/천기/LabProject07-9-5(Animation)/Player.cpp
+++ Project/천기/LabProject07-9-5(Animation)/Player.cpp
@@ -338,9 +338,8 @@ CTerrainPlayer::CTerrainPlayer(ID3D12Device *pd3dDevice, ID3D12GraphicsCommandLi
 {
 	m_pCamera = ChangeCamera(THIRD_PERSON_CAMERA, 0.0f);
 
-	CGameObject *pGameObject = CGameObject::LoadGeometryAndAnimationFromFile(pd3dDevice, pd3dCommandList, pd3dGraphicsRootSignature, "Model/CC.bin", NULL, true);
+	CGameObject *pGameObject = CGameObject::LoadGeometryAndAnimationFromFile(pd3dDevice, pd3dCommandList, pd3dGraphicsRootSignature, "Model/Default.bin", NULL, true);
 	SetChild(pGameObject);
-
 
 	CreateShaderVariables(pd3dDevice, pd3dCommandList);
 
