45d15f26e1c560ad9e770cf661cd5b2e1b7e33f8
gomtange12 <blueballoon1@naver.com>
2019-03-12 18:25:55 +0900

    server

---
+++ Project/천기/IOCPServer/IOCPServer/IOCPServer.h
@@ -0,0 +1,53 @@
+#pragma once
+#include "pch.h"
+
+
+
+#define PORT        9088
+#define BUFSIZE     1024
+#define READ        3
+#define WRITE       5
+
+///////////////////////////////////////////////
+// Structure Definition
+typedef struct          // 소켓 정보를 구조체화
+{
+	SOCKET      hClntSock;
+	SOCKADDR_IN clntAddr;
+} PER_HANDLE_DATA, *LPPER_HANDLE_DATA;
+
+typedef struct          // 소켓의 버퍼 정보를 구조체화
+{
+	OVERLAPPED overlapped;
+	CHAR       buffer[BUFSIZE];
+	WSABUF     wsaBuf;
+	int        rwMode;
+} PER_IO_DATA, *LPPER_IO_DATA;
+
+
+////////////////////////////////////////////////
+// Class definition
+class IOCPServer {
+public:
+	IOCPServer();
+	~IOCPServer();
+
+	bool Run();
+	bool Init();
+	void Cleanup();
+
+	bool setSocket();
+
+	static unsigned int __stdcall _CompletionThread(void *p_this);
+	UINT WINAPI CompletionThread();
+
+private:
+	HANDLE  m_hCompletionPort;
+
+	SOCKET m_hServSock;
+	SOCKADDR_IN m_servAddr;
+};
+
+//////////////////////////////////////////
+// Function forward declaration
+void ErrorHandling(LPCSTR pszMessage);
---
+++ Project/천기/Server/Server/FRAMEWORK.cpp
@@ -0,0 +1,17 @@
+﻿// Server.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+#include <iostream>
+//#include "stdafx.h"
+#include "IOCPServer.h"
+
+int main()
+{
+	IOCPServer *server = new IOCPServer();
+
+	server->Init();
+
+	server->Run();
+
+	delete server;
+	return 0;
+}
+
