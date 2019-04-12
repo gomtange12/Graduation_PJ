98842c4a0a065f82dcc93b43468793db4a09e925
gomtange12 <blueballoon1@naver.com>
2019-04-12 17:54:47 +0900

    쓰레드 분할

--- Project/천기/IOCPServer/IOCPServer/AcceptThread.h
+++ Project/천기/IOCPServer/IOCPServer/AcceptThread.h
@@ -1,15 +1,24 @@
 #pragma once
 #include "MyThread.h"
+#include "ObjManager.h"
 
 class AcceptThread : public MyThread
 {
+private:
+	ObjManager* objectManager = OBJMANAGER->GetObjectManager();
+
+	SOCKET listenSocket;
+	SOCKADDR_IN serverAddr;
+	SOCKET clientSocket;
+
+	int USER_NUM = 0;
 public:
+
 	explicit AcceptThread();
 	virtual ~AcceptThread();
 
 	virtual void Init();
 	virtual void Proc();
-private:
 
 };
 
--- Project/천기/IOCPServer/IOCPServer/Protocol.h
+++ Project/천기/IOCPServer/IOCPServer/Protocol.h
@@ -24,6 +24,8 @@ constexpr int SC_REMOVE_PLAYER = 3;
 constexpr int SC_MOVE_PLAYER = 4;
 constexpr int SC_MATCHING_PLAYER = 5;
 constexpr int SC_MATCHING_RESULT = 6;
+constexpr int SC_MATCH_SCENE = 7;
+
 
 struct stOverEx {
 	WSAOVERLAPPED m_wsaOver;
@@ -34,34 +36,40 @@ struct stOverEx {
 
 //////////////////////packet/////////////////////////
 struct sc_packet_pos {//SC_POSITION_INFO 3 
-	unsigned char size;
-	unsigned char type;
+	BYTE size;
+	BYTE type;
 	WORD ID;
 	WORD X_POS;
 	WORD Y_POS;
 };
 struct sc_packet_login_ok {
-	unsigned char size;
-	unsigned char type;
+	BYTE size;
+	BYTE type;
 	char id;
 };
 struct sc_packet_put_player {
-	unsigned char size;
-	unsigned char type;
+	BYTE size;
+	BYTE type;
 	char id;
-	char x, y;
+	char x;
+	char y;
 };
 struct sc_packet_remove_player {
-	unsigned char size;
-	unsigned char type;
+	BYTE size;
+	BYTE type;
 	char id;
 };
 struct sc_packet_matching {
-	unsigned char size;
-	unsigned char type;
+	BYTE size;
+	BYTE type;
 	char map;
 	char charac;
 	char mod;
+};
+struct sc_packet_match_scene {
+	BYTE size;
+	BYTE type;
+	char sceneNum;
 };
 struct sc_packet_match_result {
 	unsigned char size;
